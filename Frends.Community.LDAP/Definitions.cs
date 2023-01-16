0d71127234be3ffda9561d2c91a81090a8e3cfce
Riku Virtanen <virtanen.riku91@gmail.com>
2023-01-16 13:18:28 +0200

    PR review changes

--- Frends.Community.LDAP/Definitions.cs
+++ Frends.Community.LDAP/Definitions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -191,9 +190,7 @@ namespace Frends.Community.LDAP
                 {
                     var searchResult = new List<string>();
                     foreach (var property in SearchEntry.Properties[name])
-                    {
                         searchResult.Add(property.ToString());
-                    }
 
                     return searchResult;
                 }
